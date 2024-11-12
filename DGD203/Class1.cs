a2fa1fdf0d0c0e328f119f179d241663c901d619
MuammerBeytekin <mubyt_2005@hotmail.com>
2024-11-12 20:28:15 +0300

    Add files via upload

---
+++ DGD203/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DGD203
+{
+    public class Car
+    {
+        public Car(string color, string licenseno, string make, int year, int fuelcapacity)
+        {
+            Make = make;
+            Color = color;
+            Year = year;
+            LicenseNo = licenseno;
+            FuelCapacity = fuelcapacity;
+            Speed = 0;  
+        }
+
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string LicenseNo { get; set; }
+        public string Color { get; set; }
+        public int FuelCapacity { get; set; }
+        public int Speed { get; private set; }  
+
+        
+        public void Gas()
+        {
+            Speed += 10;
+            Console.WriteLine($"Accelerating... Current Speed: {Speed} km/h");
+        }
+
+        
+        public void Brake()
+        {
+            if (Speed >= 10)
+            {
+                Speed -= 10;
+                Console.WriteLine($"Braking... Current Speed: {Speed} km/h");
+            }
+            else
+            {
+                Speed = 0;
+                Console.WriteLine("The car is already stopped.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Make: " + Make + "\n" + "Year: " + Year + "\n" + "License No: " + LicenseNo + "\n" + "Color: " + Color + "\n" + "Fuel Capacity: " + FuelCapacity + "\n" + "Current Speed: " + Speed + " km/h";
+        }
+    }
+}
---
+++ DGD203/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using DGD203;
+
+namespace ProgramConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter Car Color: ");
+            string color = Console.ReadLine();
+
+            Console.Write("Enter License Number: ");
+            string licenseNo = Console.ReadLine();
+
+            Console.Write("Enter Country: ");
+            string make = Console.ReadLine();
+
+            Console.Write("Enter Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Fuel Capacity: ");
+            int fuelCapacity = Convert.ToInt32(Console.ReadLine());
+
+            
+            Car myCar = new Car(color, licenseNo, make, year, fuelCapacity);
+            Console.WriteLine("\nCar Details:");
+            Console.WriteLine(myCar.ToString());
+
+            
+            string input;
+            do
+            {
+                Console.WriteLine("\nChoose an action:");
+                Console.WriteLine("1 - Accelerate (Gas)");
+                Console.WriteLine("2 - Brake");
+                Console.WriteLine("3 - Show Car Details");
+                Console.WriteLine("0 - Exit");
+
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        myCar.Gas();
+                        break;
+                    case "2":
+                        myCar.Brake();
+                        break;
+                    case "3":
+                        Console.WriteLine(myCar.ToString());
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection. Please choose again.");
+                        break;
+                }
+            } while (input != "0");
+        }
+    }
+}
